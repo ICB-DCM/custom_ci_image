174e63d27f33503dbdeb1c4228b2b5d26256057e
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2020-04-03 15:54:51 +0200

    nodejs for sonar

--- install.sh
+++ install.sh
@@ -97,6 +97,8 @@ apt-get install -q -y git
 #sudo pip install -q 'awscli==1.14.64'
 
 echo "================= sonarcloud requirements ============"
+
+apt install nodejs
 export SONAR_SCANNER_VERSION=4.2.0.1873
 export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
 curl --create-dirs -sSLo $HOME/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip
